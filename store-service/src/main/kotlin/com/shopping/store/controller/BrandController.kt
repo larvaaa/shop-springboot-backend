193c1dd26587b399946f2087092843b496b0d91f
jungho <quswjdgh2@naver.com>
2026-05-13 22:32:13 +0900

    브랜드 컨트롤러 추가

---
+++ store-service/src/main/kotlin/com/shopping/store/controller/BrandController.kt
@@ -0,0 +1,16 @@
+package com.shopping.store.controller
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/brand")
+class BrandController {
+
+    @PostMapping
+    fun registerBrand() {
+
+    }
+
+}
